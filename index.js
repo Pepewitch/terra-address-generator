4b50bfc7272fc88221121d04cfa9d66d0c283fa7
Witchayut <skpepe134@gmail.com>
2021-04-20 02:15:20 +0700

    easier task

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const { MnemonicKey } = require('@terra-money/terra.js');
-const fs =  require('fs')
+const { MnemonicKey } = require("@terra-money/terra.js");
+const fs = require("fs");
 
-const target = /^terra1nkw(a|4)run/
-
-while(1) {
+const target = /^terra1nkw(a|4)/;
+const preciseTarget = /^terra1nkw(a|4)r/;
+while (1) {
   const mk = new MnemonicKey();
-  if(target.test(mk.accAddress)) {
-    fs.writeFile(`./output/${Date.now()}.json`, JSON.stringify({
-      accAddress: mk.accAddress,
-      mnemonic: mk.mnemonic
-    }))
+  if (preciseTarget.test(mk.accAddress)) {
+    fs.writeFile(
+      `./precise_output/${Date.now()}.json`,
+      JSON.stringify({
+        accAddress: mk.accAddress,
+        mnemonic: mk.mnemonic,
+      })
+    );
+  } else if (target.test(mk.accAddress)) {
+    fs.writeFile(
+      `./output/${Date.now()}.json`,
+      JSON.stringify({
+        accAddress: mk.accAddress,
+        mnemonic: mk.mnemonic,
+      })
+    );
   }
 }
